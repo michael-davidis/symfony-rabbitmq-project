199cc97bf6c46fa622902c807dd7496101d847f2
Michael Davidis <mdavidis@cluster.gr>
2022-01-09 14:48:31 +0200

    Created database insert

--- src/Service/DatabaseService.php
+++ src/Service/DatabaseService.php
@@ -2,14 +2,25 @@
 
 namespace App\Service;
 
-class DatabaseService{
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Response;
 
-    public function connectToDatabase(){
 
-    }
+class DatabaseService {
 
-    public function addEntity( $arg ){
+    public static function addEntity( ManagerRegistry $doctrine ){
+        $entityManager = $doctrine->getManager();
 
+        $product = new Product();
+        $product->setName('Keyboard');
+        $product->setPrice(1999);
+        $product->setDescription('Ergonomic and stylish!');
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($product);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
     }
 
 }
